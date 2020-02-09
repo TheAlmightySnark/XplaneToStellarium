61b921649262f161fc5ad0f06f2c4374bc0b8159
TheAlmightySnark <casdewit+ns2github@xs4all.nl>
2020-02-09 20:58:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-
-from kivy.base import runTouchApp
-from kivy.app import App
-
-from kivy.core.window import Window
-
 #Stellarium related imports
 from urllib.parse import urlencode
 import requests
@@ -17,103 +7,86 @@ import struct #structure byte arrays for X-plane
 import socket #UDP connection
 import threading #used for multithreading
 
-#RREF requsts to X-plane. position in array will determine ID send back from X-plane.
+import os
+
 xpDrefs = [
     (1, "sim/flightmodel/position/latitude", float), #ID, datarate, dataref, datatype
     (1, "sim/flightmodel/position/longitude", float), #ID, datarate, dataref, datatype
 ]
 
-sock = None
-xplaneIp = None
-xplanePort = None
+class StellariumToXplane:
+    _port = 49000
+    _IP = '127.0.0.1'
 
-#IP labels
-txtXpIP = None
-txtXpPort = None
-lblLat = None
-lblLong = None
+    _sock = None
 
-class XplaneToStellarium(App):
+    def __init__(self):
+        os.system('cls')
+        print('Welcome to Stellarium To X-plane.')
+        print('Default IP and port are 127.0.0.1 and 49000')
+        print('leave IP and PORT empty if these are fine')
 
-    def build(self):
-        global txtXpIP, txtXpPort, lblLat, lblLong
-        boxLayout = BoxLayout(orientation='vertical')
+        print('IP:')
+        if input(self):
+            self._IP = input()
 
-        txtXpIP = TextInput(text='127.0.0.1', multiline=False, size_hint_y=None, height='32dp')
-        txtXpPort = TextInput(text='49000', multiline=False, size_hint_y=None, height='32dp')
-        btnStart = Button(text="start")
-        btnStart.bind(on_press=self.onStartClickEvent)
+        print('PORT:')
+        if input(self):
+            self._port = int(input())
 
-        lblLat = Label(text='Latitude', max_lines=1, size_hint_y=None, height='32dp')
-        lblLong = Label(text='longtitude', max_lines=1, size_hint_y=None, height='32dp')
+        print('starting with %s and %s'%(self._IP, self._port))
 
-        boxLayout.add_widget(txtXpIP)
-        boxLayout.add_widget(txtXpPort)
-        boxLayout.add_widget(btnStart)
-        boxLayout.add_widget(lblLat)
-        boxLayout.add_widget(lblLong)
+        #create the socket object
+        if not self._sock:
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+        #send all the datagrams to Xplane, this tells X-plane what data to start transmitting
+        #over the UDP connection.
+        for idx, item in enumerate(xpDrefs):
+            self.requestDref(idx, item[0], item[1])
 
-        Window.size = (300, 200)
-        return boxLayout
-
-    #eventhandlers
-    def onStartClickEvent(self, instance):
-        global sock, xplaneIp, xplanePort, txtXpIP, txtPort
-        print("clicketyclick")
-
-        if not sock:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            xplaneIp = txtXpIP.text
-            xplanePort = int(txtXpPort.text)
-
-            print("opening socket with %s and %s"%(xplaneIp, xplanePort))
-
-            for idx, item in enumerate(xpDrefs):
-                self.requestDref(idx, item[0], item[1])
-
-    #build RREF according to X-plane 10/Instructions/Sending Data To X-plane.rtf
+        #build RREF according to X-plane 10/Instructions/Sending Data To X-plane.rtf
     def requestDref(self, id, rate, dref):
         padstring = '<5sii400s' #padding to 413 bytes according to the struct packing page(python)
         type = b"RREF\x00" #5 byte data type including null terminator
 
         self.sendUDP(struct.pack(padstring, type, rate, id, dref.encode()))
 
-
     def sendUDP(self, message):
-        global sock, xplaneIp, xplanePort
-
-        sock.sendto(message, (xplaneIp, xplanePort))
+        self._sock.sendto(message, (self._IP, self._port))
 
         threading.Thread(target=self.receiveUDP, args=()).start()
 
     def receiveUDP(self):
-        global sock, lblLat, lblLong
-
-        data, adress = sock.recvfrom(1024)
-
+        data, adress = self._sock.recvfrom(1024)
+        lat = None
+        lon = None
         decoded = self.decodeData(data)
 
         if decoded is None:
             print("No X-plane UDP data received")
+            os.system('cls')
             threading.Thread(target=self.receiveUDP, args=()).start()
             return
 
         for k, v in decoded.items():
             if k == 0:
-                lblLat.text = str(float(v))
+                lat = str(float(v))
                 continue
             if k == 1:
-                lblLong.text = str(float(v))
+                lon = str(float(v))
                 continue
 
-        r = requests.post("http://localhost:8090/api/location/setlocationfields", params=urlencode({'latitude': lblLat.text, 'longitude':lblLong.text}).encode())
+        r = requests.post("http://localhost:8090/api/location/setlocationfields", params=urlencode({'latitude':lat, 'longitude':lon, 'name':'X-plane'}).encode())
+
+        os.system('cls')
+        print('Latitude: %s longitude: %s'%(lat, lon))
+        print('Stellarium reply:')
         print(r.status_code, r.reason)
         print(r.url)
         print(r.text)
 
         threading.Thread(target=self.receiveUDP, args=()).start()
-
 
     def decodeData(self, source):
         #check if it's X-plane data or not
@@ -129,4 +102,4 @@ class XplaneToStellarium(App):
         return decoded
 
 if __name__ == '__main__':
-    XplaneToStellarium().run()
+    StellariumToXplane()
